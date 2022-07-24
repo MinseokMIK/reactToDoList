f01846e3c0e7e7fc4f58985d692d0191781a6fdd
Minsuk Kim <msk7316@gmail.com>
2022-07-25 01:22:46 +0900

    코딩애플 리액트 실습 마무리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
-  let [a, b] = useState('남자 코트 추천');
+  let post = '역삼 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 함수(){
+    console.log(1);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4  style={ {color : 'red' , fontSize : '16px'} }>블로그임</h4>
+        <h4  style={ {color : 'red' , fontSize : '16px'} }>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{ post }</h4>
+        <h4>{ 글제목[0]} <button onClick={()=>{
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천' 
+          글제목변경(copy); 
+          }}>변경</button> 
+          <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} 
+        </h4>
         <p>2월 17일 발행</p>
       </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <Modal/>
+
     </div>
   );
 }
+
+function Modal(){
+  return (
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
+}
+
 
 export default App;
